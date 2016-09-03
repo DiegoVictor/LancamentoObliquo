f9ab0f68c9af8a20424b2e50177c048c9a6f21fe
Diego Victor <diegovictorgonzaga@gmail.com>
2016-09-03 12:08:02 -0300

    Create and configure canvas elements

--- app/lancamento.js
+++ app/lancamento.js
@@ -6,6 +6,20 @@
 			g: 9.78033,
 			margin: 30,
 			Layers: ['Floor', 'Preview', 'Track', 'Workspace'],
+
+			config: function (canvas) {
+				canvas.height = this.height;
+				canvas.width = this.width;
+			},
+
+			Canvas: function (Layer) {
+				var canvas = document.createElement('canvas');
+				canvas.setAttribute('id', Layer);
+				this.config(canvas);
+
+				document.body.appendChild(canvas);
+				return canvas;
+			},
 		},
 		bootstrap: function () {
 		}
